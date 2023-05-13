6eaeeb696f66006020b384ce834c1b507fed4f77
tyc17afguy@gmail.com <ty@aopinc.com>
2023-05-12 18:15:03 -0700

    building

--- opnsense-filter-edit.py
+++ opnsense-filter-edit.py
@@ -1,1 +1,20 @@
-import re
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+#open csv file and create a pandas dataframe
+df = pd.read_csv('new-filters.csv')
+print (df)
+col_count = len(df.columns)
+#print each cell in the dataframe row with for loop
+for item in range(len(df)):
+    for col in range(col_count):
+        print (df.iloc[item,col])
+#print nested value from xml file
+tree = ET.parse('config.xml')
+root = tree.getroot()
+for child in root:
+    print(child.tag, child.attrib)
+    print({x.tag for x in root.findall(child.tag+"/*")})
+#get all elements and element values from XML using UUID
+for child in root.findall('OPNsense/Firewall/Alias/aliases/alias/content'):
+    print(child.tag, child.text)
